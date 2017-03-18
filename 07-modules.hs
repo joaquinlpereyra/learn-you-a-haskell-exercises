b65697d1f28ad4b1173ac5dc8750791e54898e9a
joaquinlpereyra <joaquinlpereyra@gmail.com>
2017-03-18 19:56:04 -0300

    never worked out how to convert those fucking units but hey that wasn't the important part was it

--- 07-modules.hs
+++ 07-modules.hs
@@ -3,14 +3,16 @@
 -- this will take the function for converting a distance, and an area to convert
 -- using the functions defined in the DistanceConversions module
 -- Example areaConv inchesToCentimetres 9 = 58.0644
+import DistanceConversions as DC
 
 areaConv :: (Float -> Float) -> Float -> Float
-areaConv linearConversion area = undefined
+areaConv linearConversion area = (linearConversion (area**(1/2)))**2
 
 -- define a function for converting square inches into square centimetres
 sqInToSqCm :: Float -> Float
-sqInToSqCm = undefined
+sqInToSqCm = areaConv  DC.inchesToCentimetres
 
 -- define a function for converting square chains (22 yards) to square metres
 sqChainsToSqM :: Float -> Float
-sqChainsToSqM = undefined
+sqChainsToSqM = DC.feetToInches . (areaConv (DC.yardsToFeet . (\x -> x*22)))
+
